0681fc6039100b218d5f0ed63cf691e36457a076
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-15 00:51:25 +0200

    Time: 159 ms (46.89%), Space: 54.6 MB (15.43%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> distinct;
+        for (auto i:nums){
+            distinct.insert(i);
+        }
+        
+        if (distinct.size()!=nums.size()) return true;
+        else return false;
+    }
+};
