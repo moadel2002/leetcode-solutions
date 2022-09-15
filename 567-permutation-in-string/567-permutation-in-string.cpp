a6e42e8d80aec4dcdc6b02ce03dccd0ca35625b2
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-15 20:45:24 +0200

    Time: 14 ms (58.30%), Space: 7.2 MB (74.12%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        if (s1.size()>s2.size()) return false;
         
-        int s1_size=s1.size(),s2_size=s2.size();
-        if (s1_size>s2_size) return false;
-        int freq_s1[26]={0};
-        unordered_map<int,unordered_map<int,int>> freq_s2;
-
-        for (int i=0;i<s1_size;i++){
+        int freq_s1[26]={0},freq_s2[26]={0};
+        for (int i=0;i<s1.size();i++){
             int ch_no=s1[i]-'a';
             freq_s1[ch_no]++;
         }
-
-        for (int i=1;i<=s2_size;i++){
-            int ch_no=s2[i-1]-'a';
-            for (int ch=0;ch<26;ch++){
-                freq_s2[ch][i] = freq_s2[ch][i-1];
-            }
-            freq_s2[ch_no][i] = freq_s2[ch_no][i-1]+1;
+        
+        for (int i=0;i<s1.size();i++){
+            int ch_no=s2[i]-'a';
+            freq_s2[ch_no]++;
         }
-
-        for (int st=0,en=s1_size;en<=s2_size;st++,en++){
-            bool all_found=1;
+        
+        bool all_in=true;
+        for (int i=0;i<26;i++){
+            if (freq_s1[i]!=freq_s2[i]) all_in=false;
+        }
+        
+        if (s1.size()==s2.size() or all_in){
+            return all_in;
+        }
+        
+        for (int st=1,en=s1.size();en<s2.size();st++,en++){
+            int old_ch=s2[st-1]-'a';
+            freq_s2[old_ch]--;
+            int new_ch=s2[en]-'a';
+            freq_s2[new_ch]++;
+            all_in=true;
             for (int i=0;i<26;i++){
-                if (freq_s2[i][en]-freq_s2[i][st]!=freq_s1[i]) all_found=0;
+                if (freq_s1[i]!=freq_s2[i]) all_in=false;
             }
-            if (all_found) return true;
+            if (all_in) return true;
         }
-
-        return false;
+        
+        return all_in; 
     }
 };
