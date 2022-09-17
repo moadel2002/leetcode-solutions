6bc721b52552f2ac49b9050ec4bf90e4b149b150
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-17 20:38:04 +0200

    Time: 56 ms (39.73%), Space: 25.7 MB (29.11%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int size=nums.size();
+        vector<int> new_arr(size);
+        for (int i=0;i<size;i++){
+            int new_idx=(i+k)%size;
+            new_arr[new_idx]=nums[i];
+        }
+        nums=new_arr;
+    }
+};
