e000bf0ddfbbe2d83098ee4170c5d7d4e8bbe526
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-19 14:36:09 +0200

    Time: 56 ms (22.89%), Space: 24.3 MB (5.13%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        set<int> positions;
+        for (int i=0;i<nums.size();i++){
+            if (nums[i]!=0)
+                positions.insert(i);
+        }
+        
+        for (int i=0;i<nums.size();i++){
+            if (nums[i]==0){
+                auto found=positions.upper_bound(i);
+                if (found==positions.end())
+                    break;
+                else{
+                    swap(nums[i],nums[*found]);
+                    positions.erase(found);
+                }
+            }
+        }
+        
+    }
+};
