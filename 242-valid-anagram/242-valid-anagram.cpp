beb203da5ae36737eb530c3f1f824c4c8e4fca2a
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-24 23:12:09 +0200

    Time: 4 ms (95.74%), Space: 7.4 MB (9.70%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        
+        int freqs[26],freqt[26];
+        memset(freqs,0,sizeof freqs);
+        memset(freqt,0,sizeof freqt);
+        
+        for (int i=0;i<s.size();i++){
+            freqs[s[i]-'a']++;
+        }
+        for (int i=0;i<t.size();i++){
+            freqt[t[i]-'a']++;
+        }
+        
+        for (int i=0;i<26;i++){
+            if (freqs[i]!=freqt[i])
+                return false;
+            
+        }
+        
+        return true;
     }
 };
