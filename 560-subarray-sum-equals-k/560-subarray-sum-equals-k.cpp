e88df47b4ff75be11de4458180a5abbf51a5122d
Mohamed Adel <110140891+moadel2002@users.noreply.github.com>
2022-09-25 12:56:27 +0200

    Time: 181 ms (35.46%), Space: 41.5 MB (76.79%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -5,6 +5,7 @@ public:
         int ans=0;
         unordered_map<int,int> freq;
         freq[0]=1;
+        
         for (int i=0;i<nums.size();i++){
             if (i!=0)
                 nums[i]+=nums[i-1];
